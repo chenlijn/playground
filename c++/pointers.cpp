e34b05d4d4ef8c2f22bd339557dbcb9990e69795
chenlijian <chenlijian@airdoc.com>
2018-08-08 23:41:08 +0800

    add char * codes in pointers.cpp

--- c++/pointers.cpp
+++ c++/pointers.cpp
@@ -23,15 +23,27 @@ int main(){
 
 	//cout << x <<endl;
 	
-	long arr[] = {6, 0, 9, 6};
-	long *ptr = arr;
+	//long arr[] = {6, 0, 9, 6};
+	//long *ptr = arr;
 
-	cout << *ptr <<endl;
-	ptr++;
-	cout << *ptr <<endl;
-	ptr++;
-	cout << *ptr <<endl;
-	ptr++;
-	cout << *ptr <<endl;
+	//cout << *ptr <<endl;
+	//ptr++;
+	//cout << *ptr <<endl;
+	//ptr++;
+	//cout << *ptr <<endl;
+	//ptr++;
+	//cout << *ptr <<endl;
+	//
+	
+	//string and chars
+	char courseName1[] = {'6', '.', '0', '9', '6', '\0'};
+	const char *courseName2 = "6.096"; //the const key word is needed because string literals cannot be modified.
+	cout << "1 original: " << courseName1 << endl; 
+	cout << "2 original: " << courseName2 << endl;
+	courseName1[1] = '1';
+	cout << "1 modified: " << courseName1 << endl;
+	//*(courseName2+1) = '2';
+	//cout << "2 modified: " << courseName2 << endl;
+
 	return 0;
 }
